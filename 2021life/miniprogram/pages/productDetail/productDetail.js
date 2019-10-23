64d5927b0e453744aa6f2f606a94a9f1fd8b7791
Chenghao Sun <killersun23@163.com>
2019-10-23 17:27:40 +0800

    product detail page updated, need add comments of type of input

--- 2021life/miniprogram/pages/productDetail/productDetail.js
+++ 2021life/miniprogram/pages/productDetail/productDetail.js
@@ -1,15 +1,20 @@
 // miniprogram/pages/productDetail/productDetail.js
+import {getCorrectImgUrl} from "../../functions"
+
 Page({
-
-
-
-
   /**
    * Page initial data
    */
   data: {
-    headImgPath:"",
-    detailImgPaths:[]
+    name:"",
+    price:"",
+    detailText:[],
+    variantOptionTitle:"",
+    variantOptions:[],
+    swiperImg:[],
+    detailImgPaths:[],
+    onSale:false,
+    soldOut:false,
   },
 
   /**
@@ -17,7 +22,7 @@ Page({
    */
   onLoad: function (options) {
     let that = this;
-    var imagePaths = [];
+    var correctDetailImgPaths=[];
     const cloud = wx.cloud;
 
     // console.log(options.category,options.productId)
@@ -26,6 +31,20 @@ Page({
       data: {collectionName:options.category,productId:options.productId},
       success:res=>{
         console.log("1",res);
+        let data=res.result.data;
+        let correctSwiperPaths=getCorrectImgUrl()
+
+        that.setData({
+          name: data[0].name,
+          price: data[0].price,
+          onSale: data[0].onSale,
+          soldOut: data[0].soldOut,
+          detailText: data[0].detailText.detailText,
+          variantOptionTitle: data[0].detailText.variantOptionTitle,
+          variantOptions: data[0].detailText.variantOptions,
+          swiperImg: data[0].img,
+          detailImgPaths: data[0].detailImgs,
+        })
       },fail:err=>{
         console.log("2",err);
       }
