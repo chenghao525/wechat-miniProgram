91088cf867ef9a8b9e8619666cad80329f245e94
Chenghao Sun <killersun23@163.com>
2019-10-29 15:31:59 +0800

    2021life newsite simulate product browser done

--- 2021WebSpider/2021NewSiteClickBot.py
+++ 2021WebSpider/2021NewSiteClickBot.py
@@ -1,5 +1,10 @@
 import requests
+import threading
+import urllib.request
 from bs4 import BeautifulSoup
+
+
+opener = urllib.request.build_opener()
 
 
 def getPageText(url):
@@ -19,21 +24,70 @@ def getAllCategory(pageText):
 	categoryDict = {};
 	soup = BeautifulSoup(pageText,'lxml');
 	category = soup.findAll(class_ = 'menu-item');
-	
 
-	print(category[1])
-	for a in category:
-		name = a.get_text();
-		href = a.get('href');
-		name=name.strip();
-		categoryDict.update({name:href});
-	
-	print(categoryDict)
+	for ca in category:
+		newSoup = BeautifulSoup(str(ca),'lxml');
+		for a in newSoup.findAll('a'):
+			name = a.get_text();
+			print("Getting ",name,"...");
+			href = a.get('href');
+			name=name.strip();
+			categoryDict.update({name:href});
+
 	return categoryDict;
 
 
+
+def getUsefulCategoryDict(categoryDict):
+	newDict={}
+	newDict["BEDROOM"]=categoryDict["BEDROOM"]
+	newDict["BATHROOM"]=categoryDict["BATHROOM"]
+	newDict["KITCHEN"]=categoryDict["KITCHEN"]
+	newDict["ARTISTIC"]=categoryDict["ARTISTIC"]
+	newDict["FUN"]=categoryDict["FUN"]
+	newDict["CLOTHING"]=categoryDict["CLOTHING"]
+	return newDict
+
+def simulateOpenUrl(url,category):
+	try:
+		print("Opening:",category,"==>", url)
+		opener.open(url)
+	except urllib.error.HTTPError:
+		print('urllib.error.HTTPError')
+	except urllib.error.URLError:
+		print('urllib.error.URLError')
+
+
+def browseSingleCategory(singleCategoryText,category):
+	productLinkList=[]
+	soup = BeautifulSoup(singleCategoryText,'lxml');
+	productList = soup.findAll(class_ = 'woocommerce-LoopProduct-link woocommerce-loop-product__link');
+	for pl in productList:
+		link = pl.get('href')
+		productLinkList.append(link)
+	for url in productLinkList:
+		urlThreading=threading.Thread(target = simulateOpenUrl, args =  (url,category))
+		urlThreading.start()
+
+
+def browse(newCategoryDict):
+	for key in newCategoryDict:
+		# if(key=="KITCHEN"):
+		simulateOpenUrl(newCategoryDict[key],key)
+		singleCategoryText=getPageText(newCategoryDict[key])
+		categoryThread=threading.Thread(target = browseSingleCategory, args =  (singleCategoryText,key))
+		categoryThread.start()
+
+
 if __name__ == "__main__":
-	homeUrl= "https://2021life.com";
+	times=int(input("How many times you want me to browse 2021life.com?"))
+	homeUrl = "https://2021life.com";
 	homePageText = getPageText(homeUrl);
 	categoryDict = getAllCategory(homePageText);
-	# go(categoryDict);
+	newCategoryDict = getUsefulCategoryDict(categoryDict);
+	while(times>0):
+		print("Times left: ",times)
+		simulateOpenUrl(homeUrl,"Home")
+		browse(newCategoryDict);
+		print("No error, process complete!")
+		times-=1
